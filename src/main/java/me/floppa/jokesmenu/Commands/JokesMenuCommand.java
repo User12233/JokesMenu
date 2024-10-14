9d672fc7a59c6d54bd6c5e26fafc7daa0e25a812
User12233 <90894503+User12233@users.noreply.github.com>
2024-10-14 21:10:42 +0500

    Jokesmenu v.0.7.3

--- src/main/java/me/floppa/jokesmenu/Commands/JokesMenuCommand.java
+++ src/main/java/me/floppa/jokesmenu/Commands/JokesMenuCommand.java
@@ -1,60 +1,39 @@
 package me.floppa.jokesmenu.Commands;
 
 import me.floppa.jokesmenu.Events.JokesEvents;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
 
 public class JokesMenuCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if(commandSender instanceof Player player) {
-            if (strings.length < 1 || Bukkit.getPlayer(strings[0]) == null) {
-                player.sendMessage("Использование: /jokesmenu [Ник]");
-            } else {
-                Inventory inventory = Bukkit.createInventory(player, 9 * 6);
-
-                ItemStack creeper = new ItemStack(Material.CREEPER_HEAD);
-                ItemMeta creepermeta = creeper.getItemMeta();
-                creepermeta.displayName(Component.text("§e§lЗвук крипера"));
-                creeper.setItemMeta(creepermeta);
-
-                ItemStack Barrier = new ItemStack(Material.BARRIER);
-                ItemMeta Barriermeta = Barrier.getItemMeta();
-                Barriermeta.displayName(Component.text("§4§lОграниченный"));
-                List<Component> lore = new ArrayList<>();
-                lore.add(Component.text("§4§lИгрок больше не будет взаимодействовать с"));
-                lore.add(Component.text("§4§lокружающей средой на 8 тиков"));
-                Barriermeta.lore(lore);
-                Barrier.setItemMeta(Barriermeta);
-
-                ItemStack SpawnEgg = new ItemStack(Material.ENDERMAN_SPAWN_EGG);
-                ItemMeta SpawnEggMeta = SpawnEgg.getItemMeta();
-                SpawnEggMeta.displayName(Component.text("§0§lСпавн Моба"));
-                SpawnEgg.setItemMeta(SpawnEggMeta);
-
-                inventory.setItem(10, creeper);
-                inventory.setItem(12, Barrier);
-                inventory.setItem(14, SpawnEgg);
-
-                JokesEvents.setPlayertojokee(Objects.requireNonNull(Bukkit.getPlayer(strings[0])));
-                player.openInventory(inventory);
-                player.setMetadata("Opened Menu", new FixedMetadataValue(Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("JokesMenu")), true));
+        if(commandSender instanceof Player || strings.length > 0) {
+            switch(strings[0]) {
+                case "everyone", "all", "players":
+                    activateOnAllPlayers(Bukkit.getPlayer(commandSender.getName()));
+                    break;
+                case "activated":
+                    if(JokesEvents.isActivated()) {
+                        commandSender.sendMessage("Активировано");
+                    } else {
+                        commandSender.sendMessage("Деактивировано");
+                    }
+                    break;
             }
-        } else {
-            commandSender.sendMessage("§4Consoles and something else not allowed to use this command");
+            return true;
         }
         return false;
+    }
+
+    public void activateOnAllPlayers(Player player) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            JokesEvents.randomSpawn((p.getPlayer()));
+        }
+        player.sendMessage("§aАктивирован на всех игроков");
     }
 }
